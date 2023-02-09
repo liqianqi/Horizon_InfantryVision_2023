7c0fc85be0ba177445d459b5f377409c46b15b80
liqianqi <2041671738@qq.com>
2023-02-09 14:09:09 +0800

    update readme and src

--- build/CMakeFiles/Horizon_RMVISION_2023.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for Horizon_RMVISION_2023.
-# This may be replaced when dependencies are built.
--- build/CMakeFiles/Horizon_RMVISION_2023.dir/src/autoaim/inference.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/Horizon_RMVISION_2023.dir/src/autoaim/inference.cpp.o: \
- /home/liqianqi/Horizon_InfantryVision-2023/src/autoaim/inference.cpp \
- /usr/include/stdc-predef.h
